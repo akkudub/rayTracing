62cdc8de027f560af0cfbba5f634923312226879
Akshat Dubey <akshatdubey@outlook.com>
2016-04-14 04:12:32 +0800

    added more demos, done with basic ray tracer

--- demos/demo_files/decls.js
+++ demos/demo_files/decls.js
@@ -10,7 +10,9 @@
    var fps = { startTime : 0, frameNumber : 0,
                getFPS : function() {
                   this.frameNumber++;
-                  var d = new Date().getTime(), currentTime = ( d - this.startTime ) / 1000, result = Math.floor( ( this.frameNumber / currentTime ) );
+                  var d = new Date().getTime()
+                  var currentTime = ( d - this.startTime ) / 1000
+                  var result = Math.floor( ( this.frameNumber / currentTime ) );
                   if( currentTime > 1 ) {
                      this.startTime = new Date().getTime();
                      this.frameNumber = 0;
