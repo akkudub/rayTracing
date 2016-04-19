e57ff5f04ecf1bc82e971d051741e75e05d8680a
Akshat Dubey <akshatdubey@outlook.com>
2016-04-19 10:18:07 +0800

    indented

--- project2_files/index.js
+++ project2_files/index.js
@@ -7,20 +7,20 @@ var mainCanvas = document.getElementById('mainCanvas'),
 
 mainCanvas.width = width;
 mainCanvas.height = height;
-mainCanvas.style.cssText = 'width:' + (width * 2) + 'px;height:' + (height*2) + 'px';
+mainCanvas.style.cssText = 'width:' + (width * 2) + 'px;height:' + (height * 2) + 'px';
 var ctx = mainCanvas.getContext('2d'),
     data = ctx.getImageData(0, 0, width, height);
 
 // # Throwing Rays
 function render(camera, objects, lights) {
     // first 'unpack' the arays so gpu.js can understand
-    var n_camera_point_x     = camera[0],
-        n_camera_point_y     = camera[1],
-        n_camera_point_z     = camera[2],
+    var n_camera_point_x = camera[0],
+        n_camera_point_y = camera[1],
+        n_camera_point_z = camera[2],
         n_camera_fieldOfView = camera[3],
-        n_camera_vector_x    = camera[4],
-        n_camera_vector_y    = camera[5],
-        n_camera_vector_z    = camera[6],
+        n_camera_vector_x = camera[4],
+        n_camera_vector_y = camera[5],
+        n_camera_vector_z = camera[6],
 
         n_objects_0_type = objects[0][0],
         n_objects_0_x = objects[0][1],
@@ -45,7 +45,7 @@ function render(camera, objects, lights) {
         n_objects_1_lamb = objects[1][8],
         n_objects_1_amb = objects[1][9],
         n_objects_1_rad = objects[1][10],
-        
+
         n_objects_2_type = objects[2][0],
         n_objects_2_x = objects[2][1],
         n_objects_2_y = objects[2][2],
@@ -67,30 +67,30 @@ function render(camera, objects, lights) {
         eyeVector_y = n_camera_vector_y - n_camera_point_y,
         eyeVector_z = n_camera_vector_z - n_camera_point_z;
     // unit vector
-    var eyeVector_len = Vector_length(eyeVector_x,eyeVector_y,eyeVector_z);
-        eyeVector_x /= eyeVector_len;
-        eyeVector_y /= eyeVector_len;
-        eyeVector_z /= eyeVector_len;
-   
+    var eyeVector_len = Vector_length(eyeVector_x, eyeVector_y, eyeVector_z);
+    eyeVector_x /= eyeVector_len;
+    eyeVector_y /= eyeVector_len;
+    eyeVector_z /= eyeVector_len;
+
     // cross product
     var vpRight_x = (eyeVector_y * Vector_UP_z) - (eyeVector_z * Vector_UP_y),
         vpRight_y = (eyeVector_z * Vector_UP_x) - (eyeVector_x * Vector_UP_z),
         vpRight_z = (eyeVector_x * Vector_UP_y) - (eyeVector_y * Vector_UP_x);
     // unit vector
-    var vpRight_len = Vector_length(vpRight_x,vpRight_y,vpRight_z);
-        vpRight_x /= vpRight_len;
-        vpRight_y /= vpRight_len;
-        vpRight_z /= vpRight_len;
-    
+    var vpRight_len = Vector_length(vpRight_x, vpRight_y, vpRight_z);
+    vpRight_x /= vpRight_len;
+    vpRight_y /= vpRight_len;
+    vpRight_z /= vpRight_len;
+
     // cross product
     var vpUp_x = (vpRight_y * eyeVector_z) - (vpRight_z * eyeVector_y),
-        vpUp_y =  (vpRight_z * eyeVector_x) - (vpRight_x * eyeVector_z),
-        vpUp_z =  (vpRight_x * eyeVector_y) - (vpRight_y * eyeVector_x);
+        vpUp_y = (vpRight_z * eyeVector_x) - (vpRight_x * eyeVector_z),
+        vpUp_z = (vpRight_x * eyeVector_y) - (vpRight_y * eyeVector_x);
     // unit vector
-    var vpUp_len = Vector_length(vpUp_x,vpUp_y,vpUp_z);
-        vpUp_x /= vpUp_len;
-        vpUp_y /= vpUp_len;
-        vpUp_z /= vpUp_len;
+    var vpUp_len = Vector_length(vpUp_x, vpUp_y, vpUp_z);
+    vpUp_x /= vpUp_len;
+    vpUp_y /= vpUp_len;
+    vpUp_z /= vpUp_len;
 
     var fovRadians = Math.PI * (n_camera_fieldOfView / 2) / 180,
         heightWidthRatio = height / width,
@@ -102,7 +102,7 @@ function render(camera, objects, lights) {
         pixelHeight = cameraheight / (height - 1);
 
     var index, color_R, color_G, color_B;
-    var ray_x = n_camera_point_x, 
+    var ray_x = n_camera_point_x,
         ray_y = n_camera_point_y,
         ray_z = n_camera_point_z;
 
@@ -127,35 +127,42 @@ function render(camera, objects, lights) {
                 ray_v_y = eyeVector_y + xcomp_y + ycomp_y,
                 ray_v_z = eyeVector_z + xcomp_z + ycomp_z;
             // unit vector
-            var ray_v_len = Vector_length(ray_v_x,ray_v_y,ray_v_z);
-                ray_v_x /= ray_v_len;
-                ray_v_y /= ray_v_len;
-                ray_v_z /= ray_v_len;
+            var ray_v_len = Vector_length(ray_v_x, ray_v_y, ray_v_z);
+            ray_v_x /= ray_v_len;
+            ray_v_y /= ray_v_len;
+            ray_v_z /= ray_v_len;
 
+            if (x == 39 && y == 200) {
+                debugger;
+            }
 
             color_R = trace_color0(ray_x, ray_y, ray_z, ray_v_x, ray_v_y, ray_v_z,
-                n_objects_0_type,n_objects_0_x,n_objects_0_y,n_objects_0_z,n_objects_0_r,n_objects_0_g,n_objects_0_b,n_objects_0_spec,n_objects_0_lamb,n_objects_0_amb,n_objects_0_rad,
-                n_objects_1_type,n_objects_1_x,n_objects_1_y,n_objects_1_z,n_objects_1_r,n_objects_1_g,n_objects_1_b,n_objects_1_spec,n_objects_1_lamb,n_objects_1_amb,n_objects_1_rad,
-                n_objects_2_type,n_objects_2_x,n_objects_2_y,n_objects_2_z,n_objects_2_r,n_objects_2_g,n_objects_2_b,n_objects_2_spec,n_objects_2_lamb,n_objects_2_amb,n_objects_2_rad,
+                n_objects_0_type, n_objects_0_x, n_objects_0_y, n_objects_0_z, n_objects_0_r, n_objects_0_g, n_objects_0_b, n_objects_0_spec, n_objects_0_lamb, n_objects_0_amb, n_objects_0_rad,
+                n_objects_1_type, n_objects_1_x, n_objects_1_y, n_objects_1_z, n_objects_1_r, n_objects_1_g, n_objects_1_b, n_objects_1_spec, n_objects_1_lamb, n_objects_1_amb, n_objects_1_rad,
+                n_objects_2_type, n_objects_2_x, n_objects_2_y, n_objects_2_z, n_objects_2_r, n_objects_2_g, n_objects_2_b, n_objects_2_spec, n_objects_2_lamb, n_objects_2_amb, n_objects_2_rad,
                 n_lights_x, n_lights_y, n_lights_z, 0, 0);
             color_G = trace_color0(ray_x, ray_y, ray_z, ray_v_x, ray_v_y, ray_v_z,
-                n_objects_0_type,n_objects_0_x,n_objects_0_y,n_objects_0_z,n_objects_0_r,n_objects_0_g,n_objects_0_b,n_objects_0_spec,n_objects_0_lamb,n_objects_0_amb,n_objects_0_rad,
-                n_objects_1_type,n_objects_1_x,n_objects_1_y,n_objects_1_z,n_objects_1_r,n_objects_1_g,n_objects_1_b,n_objects_1_spec,n_objects_1_lamb,n_objects_1_amb,n_objects_1_rad,
-                n_objects_2_type,n_objects_2_x,n_objects_2_y,n_objects_2_z,n_objects_2_r,n_objects_2_g,n_objects_2_b,n_objects_2_spec,n_objects_2_lamb,n_objects_2_amb,n_objects_2_rad,
+                n_objects_0_type, n_objects_0_x, n_objects_0_y, n_objects_0_z, n_objects_0_r, n_objects_0_g, n_objects_0_b, n_objects_0_spec, n_objects_0_lamb, n_objects_0_amb, n_objects_0_rad,
+                n_objects_1_type, n_objects_1_x, n_objects_1_y, n_objects_1_z, n_objects_1_r, n_objects_1_g, n_objects_1_b, n_objects_1_spec, n_objects_1_lamb, n_objects_1_amb, n_objects_1_rad,
+                n_objects_2_type, n_objects_2_x, n_objects_2_y, n_objects_2_z, n_objects_2_r, n_objects_2_g, n_objects_2_b, n_objects_2_spec, n_objects_2_lamb, n_objects_2_amb, n_objects_2_rad,
                 n_lights_x, n_lights_y, n_lights_z, 0, 1);
             color_B = trace_color0(ray_x, ray_y, ray_z, ray_v_x, ray_v_y, ray_v_z,
-                n_objects_0_type,n_objects_0_x,n_objects_0_y,n_objects_0_z,n_objects_0_r,n_objects_0_g,n_objects_0_b,n_objects_0_spec,n_objects_0_lamb,n_objects_0_amb,n_objects_0_rad,
-                n_objects_1_type,n_objects_1_x,n_objects_1_y,n_objects_1_z,n_objects_1_r,n_objects_1_g,n_objects_1_b,n_objects_1_spec,n_objects_1_lamb,n_objects_1_amb,n_objects_1_rad,
-                n_objects_2_type,n_objects_2_x,n_objects_2_y,n_objects_2_z,n_objects_2_r,n_objects_2_g,n_objects_2_b,n_objects_2_spec,n_objects_2_lamb,n_objects_2_amb,n_objects_2_rad,
+                n_objects_0_type, n_objects_0_x, n_objects_0_y, n_objects_0_z, n_objects_0_r, n_objects_0_g, n_objects_0_b, n_objects_0_spec, n_objects_0_lamb, n_objects_0_amb, n_objects_0_rad,
+                n_objects_1_type, n_objects_1_x, n_objects_1_y, n_objects_1_z, n_objects_1_r, n_objects_1_g, n_objects_1_b, n_objects_1_spec, n_objects_1_lamb, n_objects_1_amb, n_objects_1_rad,
+                n_objects_2_type, n_objects_2_x, n_objects_2_y, n_objects_2_z, n_objects_2_r, n_objects_2_g, n_objects_2_b, n_objects_2_spec, n_objects_2_lamb, n_objects_2_amb, n_objects_2_rad,
                 n_lights_x, n_lights_y, n_lights_z, 0, 2);
 
             // if (color_G > 116 && color_G < 117) {
             //     debugger;
             // }
-            debug_y[y] = {r:color_R, g:color_G, b:color_B};
+            debug_y[y] = {
+                r: color_R,
+                g: color_G,
+                b: color_B
+            };
 
             index = (x * 4) + (y * width * 4),
-            data.data[index + 0] = color_R;
+                data.data[index + 0] = color_R;
             data.data[index + 1] = color_G;
             data.data[index + 2] = color_B;
             data.data[index + 3] = 255;
@@ -169,6 +176,7 @@ var planet1 = 0,
     planet2 = 0;
 
 var f = document.querySelector("#fps");
+
 function renderLoop() {
     // make one planet spin a little bit faster than the other, just for
     // effect.
@@ -199,12 +207,12 @@ window.onload = renderLoop;
 //stooopid functions
 var selection = 0;
 
-function boost( element ) {
-  if ( element.value == "Using CPU" ) {
-     selection = 1;
-     element.value = "Using GPU";
-  } else {
-     selection = 0;
-     element.value = "Using CPU";
-  }
+function boost(element) {
+    if (element.value == "Using CPU") {
+        selection = 1;
+        element.value = "Using GPU";
+    } else {
+        selection = 0;
+        element.value = "Using CPU";
+    }
 }
